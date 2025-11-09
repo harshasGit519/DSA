dad2bfe8526c69d06fcf76693f393f1b6e13f54f
harshasGit519 <shtpulluru@gmail.com>
2025-11-10 01:01:21 +0530

    AddingExplanationOnTheIntuition

--- src/main/java/com/practice/arrays/misc/firstMissingIntegerWithoutDuplicates/FirstMissingIntegerWithoutDuplicates.java
+++ src/main/java/com/practice/arrays/misc/firstMissingIntegerWithoutDuplicates/FirstMissingIntegerWithoutDuplicates.java
@@ -12,6 +12,16 @@ public class FirstMissingIntegerWithoutDuplicates {
 
     }
 
+
+    // If the values are less than or equal to 0 -> make then N+2 because the max possible answer
+        //could be N+1 because the thing is all elements in an array could have been present
+    // Get the abs value
+    // Check if the abs value falls in the zone of the length of the array
+    // If it falls, calculate the index as current val - 1 --> This is the main intuition
+    // If the value at that index is already -ve, ignore, else, make it -ve
+    // Loop over
+    // Now, when ur out of the current loop, then iterate over again and return the current index +1 when the current index value is > 0(+ve)
+    // If nothing is found, i.e. all are negative, which means all the values pertaining that length are present. Hence, N+1
     public int firstMissingPositive(int[] A) {
         int n = A.length;
 
